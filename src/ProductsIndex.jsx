d950a25600e7da50c36804d39830b796b9c37ce5
jpashley1 <jpashley1@gmail.com>
2024-09-24 12:03:22 -0400

    added getProductData function button

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -3,13 +3,13 @@ export function ProductsIndex(props) {
   return (
     <div id="products-index">
       <h1>All Products</h1>
-      {props.products.map((product) => (
+      {/* {props.products.map((product) => (
         <div key={product.id}>
           <h2>{product.name}</h2>
           <p>{product.description}</p>
           <img src={product.image_url}></img>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
--- src/ProductsPage.jsx
+++ src/ProductsPage.jsx
@@ -3,27 +3,34 @@ import { ProductsNew } from "./ProductsNew";
 
 export function ProductsPage() {
   var firstArray = "testJPAtest";
-  var products = [
-    {
-      id: 1,
-      name: "Vic Virth Sticks",
-      description: "Description about Vic Virth Sticks",
-      image_url:
-        "https://vicfirth.com/cdn/shop/files/Drumsticks-Video-Thumbnail_65b5077c-d46d-4e26-92f2-16a879d7b6ae.jpg?v=1684500798&width=3840",
-    },
-    {
-      id: 2,
-      name: "Martin Guitar D16",
-      description: "Description about Martin Guitar",
-      image_url:
-        "https://rvb-img.reverb.com/image/upload/s--B8IansJL--/a_exif,c_limit,e_unsharp_mask:80,f_auto,fl_progressive,g_south,h_1600,q_80,w_1600/v1435852718/smii1tzgffx1iqrgb9hh.jpg",
-    },
-  ];
+  const getProductData = () => {
+    console.log("hello"); 
+  }
+  // var products = [
+  //   {
+  //     id: 1,
+  //     name: "Vic Virth Sticks",
+  //     description: "Description about Vic Virth Sticks",
+  //     image_url:
+  //       "https://vicfirth.com/cdn/shop/files/Drumsticks-Video-Thumbnail_65b5077c-d46d-4e26-92f2-16a879d7b6ae.jpg?v=1684500798&width=3840",
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Martin Guitar D16",
+  //     description: "Description about Martin Guitar",
+  //     image_url:
+  //       "https://rvb-img.reverb.com/image/upload/s--B8IansJL--/a_exif,c_limit,e_unsharp_mask:80,f_auto,fl_progressive,g_south,h_1600,q_80,w_1600/v1435852718/smii1tzgffx1iqrgb9hh.jpg",
+  //   },
+  // ];
 
   return (
     <main>
       <ProductsNew />
-      <ProductsIndex myData={firstArray} products={products} />
+      <ProductsIndex myData={firstArray} 
+      // products={products}
+      
+      />
+      <button onClick={getProductData}>get the data</button>
     </main>
   );
 }
