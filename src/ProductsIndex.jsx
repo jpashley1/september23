72deaf09a64f8087bb869100042888aaed2f593e
jpashley1 <jpashley1@gmail.com>
2024-09-24 22:19:26 -0400

    added dynamic modal

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -1,15 +1,18 @@
 export function ProductsIndex(props) {
-  console.log(props);
+
   return (
     <div id="products-index">
       <h1>All Products</h1>
-      {/* {props.products.map((product) => (
-        <div key={product.id}>
-          <h2>{product.name}</h2>
-          <p>{product.description}</p>
-          <img src={product.image_url}></img>
-        </div>
-      ))} */}
+      <div>
+        {props.products.map((product) => (
+          <div key={product.id}>
+            <h2>{product.name}</h2>
+            <img src={product.images[0].url}></img>
+            <p>Price: ${product.price} </p>
+            <button onClick={() => props.onShow(product)}>product info</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/ProductsPage.jsx
+++ src/ProductsPage.jsx
@@ -1,10 +1,20 @@
 import { ProductsIndex } from "./ProductsIndex";
 import { ProductsNew } from "./ProductsNew";
+import { Modal } from "./Modal"; 
+import  axios from 'axios';
+import { useState, useEffect } from 'react';
 
 export function ProductsPage() {
-  var firstArray = "testJPAtest";
+  // var firstArray = "testJPAtest";
+  const [products, setProducts] = useState([]);
+  const [isProductsShowVisible, setIsProductsShowVisible] = useState(false);
+  const [currentProduct, setCurrentProduct] = useState({name: "testJPAtest"});
+
+
   const getProductData = () => {
-    console.log("hello"); 
+    axios.get("http://localhost:3000/products.json").then(response => {
+      setProducts(response.data)
+    })
   }
   // var products = [
   //   {
@@ -22,15 +32,30 @@ export function ProductsPage() {
   //       "https://rvb-img.reverb.com/image/upload/s--B8IansJL--/a_exif,c_limit,e_unsharp_mask:80,f_auto,fl_progressive,g_south,h_1600,q_80,w_1600/v1435852718/smii1tzgffx1iqrgb9hh.jpg",
   //   },
   // ];
+const showModal = (product) => {
+  console.log("showing the modal");
+  console.log(product);
+  setCurrentProduct(product)
+  setIsProductsShowVisible(true)
+}
+
+const closeModal = () => {
+  console.log("closing the modal");
+  setIsProductsShowVisible(false)
+
+}
+
+useEffect(getProductData, []);
 
   return (
     <main>
+      {/* <button onClick={showModal}>Show The Modal</button> */}
+      <Modal show={isProductsShowVisible} onClose={closeModal} products={products}>
+      <p>{currentProduct.description}</p>
+        <p>TEST</p> 
+        </Modal>
       <ProductsNew />
-      <ProductsIndex myData={firstArray} 
-      // products={products}
-      
-      />
-      <button onClick={getProductData}>get the data</button>
+      <ProductsIndex products={products} onShow={showModal}/>
     </main>
   );
 }
